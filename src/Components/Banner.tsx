c3b4a715de7b644065855db2becf481706cb9dd8
Mayur505 <mayurbhuva505@gmail.com>
2023-11-30 21:01:51 +0530

    bubtton hover

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -20,8 +20,8 @@ const Banner = () => {
             </h1>
             <p className={styles.bannerSubtext}>Say goodbye to the complexities of traditional property ownership, hello to a seamless and inclusive experience</p>
             <div className={styles.mainButton}>
-              <div className={styles.BannerBuyButton}>Buy SOMI <Image src={RightArrow} alt="RightArrow" /></div>
-              <div className={styles.BannerWhitepaperButton}>Whitepaper 1.00 <Image src={RightArrowBlack} alt="RightArrowBlack" /></div>
+              <button className={styles.BannerBuyButton}>Buy SOMI <Image src={RightArrow} alt="RightArrow" /></button>
+              <button className={styles.BannerWhitepaperButton}>Whitepaper 1.00 <Image src={RightArrowBlack} alt="RightArrowBlack" /></button>
             </div>
           </div>
           <div className={styles.BannerImgSec}>
